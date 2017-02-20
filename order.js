6d195402543178c394c3cfd7c346ecfd4cafc8e0
Joshua Lerner <joshualerner@Joshuas-MacBook-Pro.local>
2017-02-20 16:57:34 -0700

    order up

--- order.js
+++ order.js
@@ -1,44 +1,47 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-// if (bTab[1].innerText === '8.99')
 $('#burgerAdd').click(function(){
-   $('.burgerTab').removeClass()
+   $('.burgerTab').toggleClass()
    $('#addyB').text((Number($('#addyB').text()) + 8.99).toFixed(2))
    $('#subTotal').text((Number($('#subTotal').text()) + 8.99).toFixed(2))
    $('#tax').text(((Number($('#subTotal').text()))* .08).toFixed(2))
    $('#total').text(((Number($('#subTotal').text()))+ Number($('#tax').text())).toFixed(2))
 })
 $('#greenAdd').click(function(){
-   $('.greenTab').removeClass()
-   $('#addyG').text((Number($('#addyG').text()) + 11.99).toFixed(2))
-   $('#subTotal').text((Number($('#subTotal').text()) + 11.99).toFixed(2))
-   $('#tax').text(((Number($('#subTotal').text()))* .08).toFixed(2))
-   $('#total').text(((Number($('#subTotal').text()))+ Number($('#tax').text())).toFixed(2))
-})
-
-$('#kalAdd').click(function(){
-   $('.kalTab').removeClass()
-   $('#addyK').text((Number($('#addyK').text()) + 14.99).toFixed(2))
+   $('.greenTab').toggleClass()
+   $('#addyG').text((Number($('#addyG').text()) + 14.99).toFixed(2))
    $('#subTotal').text((Number($('#subTotal').text()) + 14.99).toFixed(2))
    $('#tax').text(((Number($('#subTotal').text()))* .08).toFixed(2))
    $('#total').text(((Number($('#subTotal').text()))+ Number($('#tax').text())).toFixed(2))
 })
 
-$('#slurmAdd').click(function(){
-   $('.slurmTab').removeClass()
-   $('#addyS').text((Number($('#addyS').text()) + 7.99).toFixed(2))
-   $('#subTotal').text((Number($('#subTotal').text()) + 7.99).toFixed(2))
+$('#kalAdd').click(function(){
+   $('.kalTab').toggleClass()
+   $('#addyK').text((Number($('#addyK').text()) + 41.99).toFixed(2))
+   $('#subTotal').text((Number($('#subTotal').text()) + 41.99).toFixed(2))
    $('#tax').text(((Number($('#subTotal').text()))* .08).toFixed(2))
    $('#total').text(((Number($('#subTotal').text()))+ Number($('#tax').text())).toFixed(2))
 })
 
-
+$('#slurmAdd').click(function(){
+   $('.slurmTab').toggleClass()
+   $('#addyS').text((Number($('#addyS').text()) + 2.99).toFixed(2))
+   $('#subTotal').text((Number($('#subTotal').text()) + 2.99).toFixed(2))
+   $('#tax').text(((Number($('#subTotal').text()))* .08).toFixed(2))
+   $('#total').text(((Number($('#subTotal').text()))+ Number($('#tax').text())).toFixed(2))
+})
+$('.cleary').click(function(){
+   $('#subTotal').text((0.00).toFixed(2))
+   $('#tax').text((0.00).toFixed(2))
+   $('#total').text((0.00).toFixed(2))
+   $('#addyB').text((0.00).toFixed(2))
+   $('#addyG').text((0.00).toFixed(2))
+   $('#addyK').text((0.00).toFixed(2))
+   $('#addyS').text((0.00).toFixed(2))
 })
 
-// $ ('p').on('click' function(){
-//    console.log('<p> clicked, i win');
-// })
+$('.placeyPlace').click(function(){
+   alert('our fastest birds are on their way')
+})
 
-// $('p').click(function(){
-//    $(this).toggleClass('active')
-// })
+})
